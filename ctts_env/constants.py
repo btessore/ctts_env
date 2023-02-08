683acf89182f210e366c26217521111ec37957d7
Benjamin Tessore <benjamin.tessore@univ-grenoble-alpes.fr>
2023-02-08 15:44:08 +0100

    adding Jupiter units

--- ctts_env/constants.py
+++ ctts_env/constants.py
@@ -12,10 +12,14 @@ sigma = 5.670367e-8
 Msun = 1.3271244e20 / Ggrav
 Rsun = 6.957e8  # m
 Lsun = 3.828e26  # W
+Mj = 1.898e27  # kg = 1.2668653e17 / 6.67428e-11 = Ggrav
+Rj = 69911e3  # m
 
 
 yr_to_sec = 3.154e7  # sec / yr
 Msun_per_year_to_SI = Msun / yr_to_sec  # Msun / 31557600
 day_to_sec = 86400.0  # number of seconds in a day
+Rj_to_Rsun = Rj / Rsun
+Mj_per_year_to_SI = Mj / yr_to_sec
 
 tiny_val = 1e-50  # prevents division by 0
